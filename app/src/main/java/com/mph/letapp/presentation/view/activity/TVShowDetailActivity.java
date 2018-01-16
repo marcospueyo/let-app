90dc51f4d51feae0c6b5ab8d405c50cb9ce93d5b
Marcos Pueyo <marcos.pueyo@est.fib.upc.edu>
2018-01-16 11:01:10 +0100

    - TVShowDetailActivity renders a tvshowviewmodel upon Presenter's request.

--- app/src/main/java/com/mph/letapp/presentation/view/activity/TVShowDetailActivity.java
+++ app/src/main/java/com/mph/letapp/presentation/view/activity/TVShowDetailActivity.java
@@ -3,8 +3,17 @@ package com.mph.letapp.presentation.view.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
 import com.mph.letapp.R;
 import com.mph.letapp.di.activity.ActivityComponent;
 import com.mph.letapp.di.activity.DaggerActivity;
@@ -12,7 +21,14 @@ import com.mph.letapp.presentation.TVShowDetailView;
 import com.mph.letapp.presentation.model.TVShowViewModel;
 import com.mph.letapp.presentation.presenter.TVShowDetailPresenter;
 
+import java.util.Locale;
+
 import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
 
 public class TVShowDetailActivity extends DaggerActivity implements TVShowDetailView {
 
@@ -21,10 +37,22 @@ public class TVShowDetailActivity extends DaggerActivity implements TVShowDetail
     @SuppressWarnings("unused")
     public static final String TAG = TVShowDetailActivity.class.getSimpleName();
 
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+
+    @BindView(R.id.iv_logo) ImageView ivLogo;
+
+    @BindView(R.id.tv_title) TextView tvTitle;
+
+    @BindView(R.id.tv_description) TextView tvDescription;
+
+    @BindView(R.id.tv_rating) TextView tvRating;
+
     @Inject
     TVShowDetailPresenter mPresenter;
 
     private String mShowID;
+
+    private RequestManager mRequestManager;
 
     public static Intent getNewIntent(Context context, String tvShowID) {
         Intent intent = new Intent(context, TVShowDetailActivity.class);
@@ -36,8 +64,10 @@ public class TVShowDetailActivity extends DaggerActivity implements TVShowDetail
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tvshow_detail);
+        ButterKnife.bind(this);
         loadShowDetails();
         initializePresenter();
+        initializeRequestManager();
     }
 
     @Override
@@ -71,21 +101,26 @@ public class TVShowDetailActivity extends DaggerActivity implements TVShowDetail
     @Override
     public void showTVShow(TVShowViewModel tvShow) {
         setTitle(tvShow.title());
+        renderLogo(tvShow.image());
+        renderTitle(tvShow.title());
+        renderDescription(tvShow.description());
+        renderRating(tvShow.rating());
     }
 
     @Override
     public void showProgress() {
-
+        mProgressBar.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hideProgress() {
-
+        mProgressBar.setVisibility(View.GONE);
     }
 
     @Override
     public void showLoadError() {
-        Log.d(TAG, "showLoadError: ");
+        Snackbar.make(findViewById(android.R.id.content),
+                getString(R.string.entity_detail_load_error), Snackbar.LENGTH_SHORT).show();
     }
 
     private void loadShowDetails() {
@@ -98,5 +133,35 @@ public class TVShowDetailActivity extends DaggerActivity implements TVShowDetail
         if (mShowID != null && mPresenter != null) {
             mPresenter.initialize(mShowID);
         }
+    }
+
+    private void initializeRequestManager() {
+        mRequestManager = Glide.with(this);
+    }
+
+    private void renderLogo(String image) {
+        RequestOptions requestOptions = new RequestOptions()
+                .error(ContextCompat.getDrawable(this, R.drawable.ic_tv_placeholder));
+        mRequestManager
+                .load(image)
+                .apply(requestOptions)
+                .transition(withCrossFade())
+                .into(ivLogo);
+    }
+
+    private void renderTitle(String title) {
+        tvTitle.setText(title);
+    }
+
+    private void renderRating(Double rating) {
+        tvRating.setText(getFormattedRating(rating));
+    }
+
+    private void renderDescription(String description) {
+        tvDescription.setText(description);
+    }
+
+    private String getFormattedRating(Double rating) {
+        return String.format(Locale.getDefault(), "%.1f", rating) + " / 10";
     }
 }
